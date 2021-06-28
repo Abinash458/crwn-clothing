52fe8e904c4777891e379e271cdd41fe9730565d
Abinash Mohapatra <abinash@expensebit.com>
2021-06-28 16:27:33 +0530

    integrate redux-persists to save the cartitem

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom"
+import { PersistGate } from "redux-persist/integration/react";
 
 import './index.css';
 import App from './App';
-import store from './redux/store';
+import { persistor, store } from './redux/store';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
