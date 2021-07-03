3edec831d3f52c895d7325e81099eebf9fac0c0e
Abinash Mohapatra <abinash@expensebit.com>
2021-07-03 16:54:18 +0530

    implement code that pulls in shop data from the firestore and stores in into the shop reducer from out shop page. Also inculded withSpinner HOC

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from "react-router-dom"
+import { connect } from 'react-redux';
+
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
 import CollectionOverview from '../../components/collections-overview/collections-overview.components';
 import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ match }) => {
-    return (
-        <div className="shop-page">
-            <Route exact path={`${match.path}`} component={CollectionOverview} />
-            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-        </div>
-    );
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { updateCollections } from '../../redux/shop/shop.actions';
+
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+class ShopPage extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            loading: true
+        }
+    }
+    
+    
+    unsubscribeFromSnapshot = null;
+
+    componentDidMount() {
+
+        const { updateCollections } = this.props;
+
+        const collectionRef = firestore.collection('collections');
+
+        collectionRef.get().then(snapshot => {
+            const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionMap);
+            this.setState({ loading: false })
+        })
+    }
+
+    render() {
+        const { match } = this.props;
+        return (
+            <div className="shop-page">
+                <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={this.state.loading} {...props} />} />
+                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={this.state.loading} {...props} />} />
+            </div>
+        );
+    }
 }
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+})
+
+export default connect(null, mapDispatchToProps)(ShopPage);
