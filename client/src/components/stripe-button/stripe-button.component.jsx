cb5358f0af3513b73fdca3d5e65dfe28baac2f3f
Abinash Mohapatra <abinash@expensebit.com>
2021-07-18 12:34:53 +0530

    remove gitignore files

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = "pk_test_51I0gAOCtZqYd13Ac2yidlvw0KLZ4sxK6SSmBnQ9Lk908YBly8EgYodvVyYtUYVe3xoaNGWz9ZWqCUS80Me6CdKH2001XRbA0lJ";
 
     const onToken = token => {
-        console.log(token);
-        alert("Payment Successfull")
+        // console.log(token);
+        axios({
+            url: '/payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment Successful');
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert('There was an issue with your payment, Please sure you use the provided credit card.');
+        })
     }
 
     return (
