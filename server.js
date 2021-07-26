512ec4c1f6e87b4897fc2d37b5260bf663a40468
Abinash Mohapatra <abinash@expensebit.com>
2021-07-26 13:43:35 +0530

    changed server.js file to run in local

--- server.js
+++ server.js
@@ -11,24 +11,24 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
-
-if(process.env.NODE_ENV === 'production') {
-    app.use(enforce.HTTPS({ trustProtoHeader: true }));
-    app.use(express.static(path.join(__dirname, 'client/build')));
-
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
+ 
+if (process.env.NODE_ENV === 'production') {
+  app.use(compression);
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+ 
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
-
+ 
 app.listen(port, error => {
-    if(error) throw error;
-    console.log(`Server running on PORT ${port}`);
+  if (error) throw error;
+  console.log('Server is running on port ' + port);
 });
+
 
 app.get('/service-worker.js', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'))
